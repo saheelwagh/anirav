efb9e6729f8b5f99e97ed084e19b9663368d9848
VRPP CO-ORDINATOR <RAMRAO@VRPPs-MacBook-Air.local>
2019-11-19 17:30:56 +0530

    old version works

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
+//import and set up express server
 const express = require("express");
 const app = express();
-
+//body parser -> may not be required
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
-
+//passport. delete unless necessary
 const passport = require("passport");
-
+//make login page available
 app.use(express.static("public"));
-
+//serve index file
 app.get("/", (req, res) => res.send("./index2.html"));
+// check login.
 app.post("/submit-form", (req, res) => {
   const password = req.body.password;
   console.log(password);
   if (password === "anirav") {
+    //if successful make assets available
     app.use(express.static("assets"));
+    //load fist main page for authenticated users. it will be avilable henceforth till server sleeps
     res.redirect("./main.html");
   } else {
     res.redirect("/");
